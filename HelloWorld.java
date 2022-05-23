d2ae8fd48ef877833c57f80da987e4873c110ae9
@hutch_mathers <Daniel.H.Mathers@leidos.com>
2022-05-23 14:09:05 -0500

    Remove a couple of '-'s

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("----***Hello world and welcome to Jenkins training!***----");
+		System.out.println("--***Hello world and welcome to Jenkins training!***----");
 	}
 }
+
+//localhost:8080/job/HelloWorld/build?token=token123
